c8d1ae27b0e6c6f2c810374c427925ead4f5f732
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-01-13 14:03:56 +0100

    cambios en viajeros

--- viajeros/src/viajeros/App.java
+++ viajeros/src/viajeros/App.java
@@ -5,25 +5,30 @@ public class App {
 	public static void main(String[] args) {
 		Viajeros V1 = new Viajeros();
 		Viajeros V2 = new Viajeros("12345678U","Pepe", "Castillo Galan", "Español", "Camino de la muerte Nº666");
-		System.out.println(V2.getDNI());
+		Viajeros V3 = new Viajeros();
+		/*System.out.println(V2.getDNI());
 		System.out.println(V2.getNombre());
 		System.out.println(V2.getApellidos());
 		System.out.println(V2.getNacionalida());
-		System.out.println(V2.getDireccion());
-		System.out.println(V2.getInfo());
+		System.out.println(V2.getDireccion());*/
+		
 		
 		V1.setNombre("Mr.West");
 		V1.setApellidos("Kanie Frederick");
-		V1.setDNI("FLACKITO9");
 		V1.setNacionalidad("Flaquitolandia");
 		V1.setDireccion("West Cementerio");
 		
-		System.out.println(V1.getDNI());
+		/*System.out.println(V1.getDNI());
 		System.out.println(V1.getNombre());
 		System.out.println(V1.getApellidos());
 		System.out.println(V1.getNacionalida());
-		System.out.println(V1.getDireccion());
-		System.out.println(V1.getInfo());
+		System.out.println(V1.getDireccion());*/
+		System.out.println(V1.toString());
+		System.out.println(" ");
+		System.out.println(V2.toString());
+		System.out.println(" ");
+		System.out.println(V3.toString());
+		System.out.println(V1.getContador());
 	}
 
 }
--- viajeros/src/viajeros/Viajeros.java
+++ viajeros/src/viajeros/Viajeros.java
@@ -3,14 +3,18 @@ package viajeros;
 import java.util.Arrays;
 
 public class Viajeros {
+	private static int contador =0;
+	private int id;
 	private String DNI;
 	private String Nombre;
 	private String Apellidos;
 	private String Nacionalidad;
 	private String Direccion;
-	private String info = this.DNI+" "+this.Nombre+" "+this.Apellidos+" "+this.Nacionalidad+" "+this.Direccion;
+	private String info;
 
 	public Viajeros() {
+		contador++;
+		this.id = contador;
 		DNI = "00000000X";
 		Nombre = "Tu nombre";
 		Apellidos = "Tus apellidos";
@@ -20,12 +24,14 @@ public class Viajeros {
 	}
 
 	public Viajeros(String DNI, String Nombre, String Apellidos, String Nacionalidad, String Direccion) {
+		contador++;
+		this.id = contador;
 		this.DNI = DNI;
 		this.Direccion = Direccion;
 		this.Nombre = Nombre;
 		this.Apellidos = Apellidos;
 		this.Nacionalidad = Nacionalidad;
-		info = this.DNI+" "+this.Nombre+" "+this.Apellidos+" "+this.Nacionalidad+" "+this.Direccion;
+		
 		
 	}
 
@@ -39,6 +45,8 @@ public class Viajeros {
 			return;
 		}
 	}
+	public int getId() {return id;}
+	public int getContador(){return contador;}
 	public String getNombre() {return this.Nombre;}
 	public String getApellidos(){return this.Apellidos;}
 	public String getNacionalida() {return this.Nacionalidad;}
@@ -49,10 +57,10 @@ public class Viajeros {
 	public void setNombre(String Nombre){this.Nombre=Nombre;}
 	public void setApellidos(String Apellidos){this.Apellidos=Apellidos;}
 	public void setNacionalidad(String Nacionalidad){this.Nacionalidad=Nacionalidad;}
-	public void setDNI(String DNI){this.DNI=DNI;} 
 	public void setInfo(String DNI, String Nombre, String Apellidos, String Nacionalidad, String Direccion) {info = this.DNI+" "+this.Nombre+" "+this.Apellidos+" "+this.Nacionalidad+" "+this.Direccion;}
 	
-	public String getInfo() {
+	public String toString() {
+		info = "Hola soy "+ Nombre +" "+Apellidos+" soy de "+Nacionalidad+"\n Vivo en "+Direccion+" mi DNI es "+DNI+" y soy el viajero "+id+" en total somos "+contador;
 		return info;
 	}
 
