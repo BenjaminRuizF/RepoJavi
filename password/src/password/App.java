e99c456db951474867b5da1580a9c382167f1a60
b.ruiz <b.ruiz@2DAW-13.2SMR.GSD.local>
2023-01-26 11:47:23 +0100

    Cambios en password

--- password/src/password/App.java
+++ password/src/password/App.java
@@ -3,8 +3,14 @@ package password;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Password P1 = new Password();
+		Password P2 = new Password(9);
+		P1.setLongitud(8);
+		System.out.println(P1.getContrasenia());
+		while(P1.esFuerte()!=true) {
+			P1.generarPassword();
+			System.out.println(P1.getContrasenia());
+		}
 	}
 
 }
--- password/src/password/Password.java
+++ password/src/password/Password.java
@@ -2,7 +2,7 @@ package password;
 
 public class Password {
 	private int longitud;
-	private String contrasenia;
+	private String contrasenia = "";
 	public Password() {
 		longitud = 8;
 		contrasenia = generarPassword();
@@ -11,9 +11,55 @@ public class Password {
 		this.longitud = longitud;
 		contrasenia = generarPassword();
 	}
+	public boolean esFuerte() {
+		int contadorMayus = 0;
+		int contadorMinus = 0;
+		int contadorNum = 0;
+		String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String minusculas = "abcdefghijklmnopqrstuvwxyz";
+		String numeros = "123456789";
+		for(int i= 0;i<contrasenia.length();i++) {
+			for (int j = 0; j < mayusculas.length(); j++) {
+				if(contrasenia.charAt(i)==mayusculas.charAt(j)) {
+					contadorMayus++;
+				}
+			}
+		}
+		for(int i= 0;i<contrasenia.length();i++) {
+			for (int j = 0; j < minusculas.length(); j++) {
+				if(contrasenia.charAt(i)==minusculas.charAt(j)) {
+					contadorMinus++;
+				}
+			}
+		}
+		for(int i= 0;i<contrasenia.length();i++) {
+			for (int j = 0; j < numeros.length(); j++) {
+				if(contrasenia.charAt(i)==numeros.charAt(j)) {
+					contadorNum++;
+				}
+			}
+		}
+		if(contadorMayus<2||contadorMinus<1||contadorNum<5) {
+			return false;
+		}
+		return true;
+	}
 	public String generarPassword(){
-		
-		return "hola";
+		String aleatorio = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		while(contrasenia.length() < longitud) {
+			int num = (int)(Math.random()*63);
+			contrasenia += aleatorio.charAt(num);
+		}
+		return contrasenia;
+	}
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+	public String getContrasenia() {
+		return contrasenia;
 	}
 	
 }
